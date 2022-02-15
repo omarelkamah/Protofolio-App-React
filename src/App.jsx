02c4d2adf09fdecb79500b7984774c256e2a2a4c
Omar Ahmed <omarwork010@gmail.com>
2022-02-15 17:11:51 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+// import './App.scss';
+
+import './app.css';
+
+import Contact from "./components/contact/Contact";
+import Home from './components/home/Home';
+import Menue from './components/menue/Menue';
+import Navbar from "./components/NavBar/Navbar";
+import Protofolio from './components/protofolio/Protofolio';
+import Testimonials from "./components/testimonials/Testimonials";
+import Works from "./components/works/Works";
+
+
+
+function App() {
+  const [openMenue, handeleMenue] = useState(false);
+  
+  return (
+    <div className="App">
+      <Navbar openMenue={openMenue} handeleMenue={handeleMenue} />
+      <Menue openMenue={openMenue} handeleMenue={handeleMenue} />
+      <div className="sectionsContainer">
+        <Home />
+        <Protofolio />
+        <Works />
+        <Testimonials />
+        <Contact />
+      </div>
+    </div>
+  );
+}
+
+export default App;
